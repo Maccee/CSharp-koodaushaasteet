4057e02cc53f4652c4338cef1d91819ceae44d77
Macce <makkeeh@gmail.com>
2023-04-11 11:57:47 +0300

    haaste3

---
+++ Ohjelmointihaaste3-Ritariryhma_liikkeelle/Ritariryhma_liikkeelle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Ritariryhma_liikkeelle
+{
+
+
+    static void Main(string[] args)
+    {
+        Console.Title = "Ritariryhmä Liikkeelle";
+
+        int rivi = VainNumero("Anna kohde rivi (1-8): ");
+        int sarake = VainNumero("Anna kohde sarake (1-8): ");
+        
+
+        int[] riviMuutos = { -1, 1, 0, 0 };
+        int[] sarakeMuutos = { 0, 0, -1, 1 };
+
+        Console.WriteLine("Lähetetään ritarit.");
+        Console.ForegroundColor = ConsoleColor.Red;
+
+        for (int i = 0; i < 4; i++)
+        {
+            int uusiRivi = rivi + riviMuutos[i];
+            int uusiSarake = sarake + sarakeMuutos[i];
+
+            if (uusiRivi >= 1 && uusiRivi <= 8 && uusiSarake >= 1 && uusiSarake <= 8)
+            {
+                Console.WriteLine($"Ritari {i + 1} kohteeseen ({uusiRivi}, {uusiSarake})");
+                //Console.Beep();
+            }
+        }
+
+        Console.ResetColor();
+        //Console.ReadKey();
+    }
+
+    static int VainNumero(string prompt)
+    {
+        while (true)
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int value) && value >= 1 && value <= 8)
+            {
+                return value;
+            }
+            
+        }
+    }
+}
