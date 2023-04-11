d9faa104e714dc5d1f48014ce365f7f77fd6fa22
Macce <makkeeh@gmail.com>
2023-04-11 14:12:20 +0300

    haaste6

---
+++ Ohjelmointihaaste6-Taikakanuuna/Taikakanuuna.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+class Taikakanuuna
+{
+    static void Main(string[] args)
+    {
+        string ammus = "";
+        while (true)
+        {
+            Console.Clear();
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.ResetColor();
+                ammus = "Normal";
+
+                if (i % 3 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    ammus = "Fire";
+                }
+
+                if (i % 5 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    ammus = "Electric";
+                }
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    ammus = "Electric and Fire";
+                }
+
+                Console.WriteLine($"{i}: {ammus}");
+                Thread.Sleep(20);
+
+            }
+            Console.ResetColor();
+            ConsoleKeyInfo keyInfo;
+            Console.Write("Ammukset loppu! Ladataanko ase? (y/n): ");
+            while (true)
+            {
+                keyInfo = Console.ReadKey();
+                if (keyInfo.KeyChar == 'n' || keyInfo.KeyChar == 'y')
+                {
+                    break;
+                }
+            }
+
+            if (keyInfo.KeyChar == 'n')
+            {
+                break;
+            }
+        }
+
+
+    }
+}
