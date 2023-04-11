9a61146cd26b1e3bb0399cd3544e9affcebc98e3
Macce <makkeeh@gmail.com>
2023-04-11 12:20:56 +0300

    haaste4

---
+++ Ohjelmointihaaste4-Vartiotorni/Vartiotorni.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class Vartiotorni
+{
+    static void Main(string[] args)
+    {
+        int x = VainNumero("Anna x-arvo: ");
+        int y = VainNumero("Anna y-arvo: ");
+
+
+        string suunta = "!";
+
+        if (y > 0)
+        {
+            suunta += "north";
+        }
+        else if (y < 0)
+        {
+            suunta += "south";
+        }
+
+        if (x > 0)
+        {
+            suunta += suunta.Length > 0 ? " east." : "east.";
+        }
+        else if (x < 0)
+        {
+            suunta += suunta.Length > 0 ? " west." : "west.";
+        }
+
+        Console.WriteLine($"The enemy is to the {suunta}");
+    }
+    static int VainNumero(string prompt)
+    {
+        while (true)
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int value))
+            {
+                return value;
+            }
+
+        }
+    }
+}
