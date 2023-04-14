a4528aab062c5d09479cde208caaf73e8fb8989a
Macce <makkeeh@gmail.com>
2023-04-14 12:21:53 +0300

    haaste10

---
+++ Ohjelmointihaaste10-PIN_ja_PUK_koodit/PIN_ja_PUK_koodit.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class PIN_ja_PUK_koodit
+{
+    static void Main(string[] args)
+    {
+        int pinAttempts = 0;
+        int pukAttempts = 0;
+        bool pinUnlocked = false;
+        string correctPin = "1234";
+        string correctPuk = "12345678";
+
+        while (pinAttempts < 3 && !pinUnlocked)
+        {
+            Console.Write("Anna nelinumeroinen PIN-koodi: ");
+            string? inputPin = Console.ReadLine();
+
+            if (inputPin == correctPin)
+            {
+                pinUnlocked = true;
+                Console.WriteLine("PIN-koodi on oikein!");
+            }
+            else
+            {
+                pinAttempts++;
+                Console.WriteLine("PIN-koodi on väärin!");
+            }
+        }
+
+        if (!pinUnlocked)
+        {
+            while (pukAttempts < 3 && !pinUnlocked)
+            {
+                Console.Write("Anna kahdeksannumeroinen PUK-koodi: ");
+                string? inputPuk = Console.ReadLine();
+
+                if (inputPuk == correctPuk)
+                {
+                    pinUnlocked = true;
+                    Console.WriteLine("PUK-koodi on oikein, laite on avattu!");
+                }
+                else
+                {
+                    pukAttempts++;
+                    Console.WriteLine("PUK-koodi on väärin!");
+                }
+            }
+
+            if (!pinUnlocked)
+            {
+                Console.WriteLine("Laite on pysyvästi lukittu.");
+            }
+        }
+    }
+    
+}
